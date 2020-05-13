7f05c9f31ceef997eabe91762f2605d8067e3c7b
Athou <jeremiepanzer@gmail.com>
2020-05-13 11:10:46 +0200

    cleanup

--- client/src/components/characters/CharacterCard.tsx
+++ client/src/components/characters/CharacterCard.tsx
@@ -1,39 +1,35 @@
 import { Button, Card, H5, Icon } from "@blueprintjs/core"
 import React from "react"
-import styled from "styled-components"
+import { Box, Flex } from "reflexbox"
 import { UserCharacter } from "../../api"
 import { characterSpecLabels } from "../../api/utils"
 import { SpecIcon } from "../spec-icons/SpecIcon"
-
-const CardTitle = styled(H5)`
-    display: flex;
-`
-
-const CardTitleText = styled.div`
-    flex-grow: 1;
-`
-
-const StyledCharacterSpec = styled.span`
-    margin-left: 0.2rem;
-`
-
-const StyledMainIcon = styled(Icon)`
-    margin-right: 0.4rem;
-`
 
 export const CharacterCard: React.FC<{ character: UserCharacter; editable: boolean; onEdit: () => void }> = props => {
     return (
         <>
             <Card elevation={2}>
-                <CardTitle>
-                    {props.character.main && <StyledMainIcon icon="tick-circle" iconSize={18} />}
-                    <CardTitleText>{props.character.name}</CardTitleText>
-                    {props.editable && <Button icon="edit" onClick={props.onEdit} />}
-                </CardTitle>
-                <div>
-                    <SpecIcon spec={props.character.spec} />
-                    <StyledCharacterSpec>{characterSpecLabels[props.character.spec]}</StyledCharacterSpec>
-                </div>
+                <Flex>
+                    {props.character.main && (
+                        <Box mr={2}>
+                            <Icon icon="tick-circle" iconSize={18} />
+                        </Box>
+                    )}
+                    <Box flexGrow={1}>
+                        <H5>{props.character.name}</H5>
+                    </Box>
+                    {props.editable && (
+                        <Box>
+                            <Button icon="edit" onClick={props.onEdit} />
+                        </Box>
+                    )}
+                </Flex>
+                <Flex>
+                    <Box mr={2}>
+                        <SpecIcon spec={props.character.spec} />
+                    </Box>
+                    <Box>{characterSpecLabels[props.character.spec]}</Box>
+                </Flex>
             </Card>
         </>
     )
