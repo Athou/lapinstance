7f649107c42cb8dec0934aefe77d3ba4396cea45
Athou <jeremiepanzer@gmail.com>
2020-09-28 15:22:42 +0200

    secure websockets

--- server/src/main/java/be/hehehe/lapinstance/WebSocketConfiguration.java
+++ server/src/main/java/be/hehehe/lapinstance/WebSocketConfiguration.java
@@ -2,13 +2,14 @@ package be.hehehe.lapinstance;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfiguration extends AbstractSecurityWebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -19,6 +20,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/ws").setAllowedOrigins("*");
+	}
+
+	@Override
+	protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+		messages.anyMessage().hasAuthority(UserRole.USER.name());
+	}
+
+	@Override
+	protected boolean sameOriginDisabled() {
+		return true;
 	}
 
 }
