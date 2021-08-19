4b97ab4990ffb34bc6bdf6722b3ab16978d2d65e
Jérémie Panzer <jeremie.panzer@erasme.ulb.ac.be>
2021-08-19 11:48:05 +0200

    remove disabled users from impersonate list

--- client/src/components/UserPicker.tsx
+++ client/src/components/UserPicker.tsx
@@ -4,6 +4,7 @@ import _ from "lodash"
 import React, { useEffect, useState } from "react"
 import { User } from "../api"
 import { client } from "../api/client"
+import { useSession } from "../hooks/useSession"
 
 const UserSelect = Select.ofType<User>()
 const normalizeForSearch = (str: string) => _.deburr(str).toLowerCase()
@@ -12,12 +13,17 @@ export const UserPicker: React.FC<{
     userId: number
     onChange: (id: number) => void
 }> = props => {
+    const session = useSession()
     const [users, setUsers] = useState<User[]>()
     const selectedUser = users?.find(u => u.id === props.userId)
 
     useEffect(() => {
-        client.users.findAllUsers().then(resp => setUsers(_.sortBy(resp.data, u => u.name)))
-    }, [])
+        client.users.findAllUsers().then(resp => {
+            const activeUsers = resp.data.filter(u => u.id === session.user.id || !u.disabled)
+            const sortedActiveUsers = _.sortBy(activeUsers, u => u.name.toLowerCase())
+            setUsers(sortedActiveUsers)
+        })
+    }, [session.user.id])
 
     return (
         <>
