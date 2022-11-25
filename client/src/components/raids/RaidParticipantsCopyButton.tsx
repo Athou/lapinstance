ffafbbc953991af419154a60cc47fa4149b60090
Athou <jeremiepanzer@gmail.com>
2022-11-25 08:15:57 +0100

    add more info to copy button to ease paste in excel

--- client/src/components/raids/RaidParticipantsCopyButton.tsx
+++ client/src/components/raids/RaidParticipantsCopyButton.tsx
@@ -1,12 +1,38 @@
 import { Toaster } from "@blueprintjs/core"
+import _ from "lodash"
 import React from "react"
 import { RaidSubscription } from "../../api"
+import { CharacterRole, specToRoleMapping } from "../../api/utils"
 import { ActionButton } from "../ActionButton"
 
 const toaster = Toaster.create()
 export const RaidParticipantsCopyButton: React.FC<{ subscriptions: RaidSubscription[] }> = props => {
     const copyButtonClicked = () => {
-        const text = props.subscriptions.map(sub => sub.character?.name).join(",")
+        const sortedSubscriptions = _.sortBy(
+            props.subscriptions,
+            s => {
+                const spec = s.character?.spec
+                if (!spec) return 99
+
+                const role = specToRoleMapping[spec]
+                if (role === CharacterRole.TANK) return 0
+                if (role === CharacterRole.HEAL) return 1
+                return 2
+            },
+            s => s.character?.spec
+        )
+        const text = sortedSubscriptions
+            .map(sub => {
+                const lines = [sub.character?.name]
+                const spec = sub.character?.spec
+                if (spec) {
+                    const role = specToRoleMapping[spec]
+                    if (role === CharacterRole.TANK || role === CharacterRole.HEAL) lines.push(role)
+                    else lines.push("DPS")
+                }
+                return lines.join("\t")
+            })
+            .join("\n")
         navigator.clipboard.writeText(text).then(
             () =>
                 toaster.show({
